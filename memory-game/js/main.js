1e756753a2e373df0ee5695b40f3bec4581572ca
karthikeyansekar <karthikeyansep27@gmail.com>
2020-01-24 20:37:06 +1100

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "king";
+console.log("user flipped" + cardOne);
+console.log("user flipped" + cardTwo);
+console.log("user flipped" + cardThree);
+console.log("user flipped" + cardFour);
