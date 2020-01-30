405535bd0290cf44633babcbd5f06f2ef240e8bb
karthikeyansekar <karthikeyansep27@gmail.com>
2020-01-30 20:39:32 +1100

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,4 +1,4 @@
-console.log("Up and running!");
+/*console.log("Up and running!");
 let cardOne = "queen";
 let cardTwo = "queen";
 let cardThree = "king";
@@ -6,4 +6,25 @@ let cardFour = "king";
 console.log("user flipped" + cardOne);
 console.log("user flipped" + cardTwo);
 console.log("user flipped" + cardThree);
-console.log("user flipped" + cardFour);
+console.log("user flipped" + cardFour);*/
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+var cardOne = "queen";
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+var cardTwo = "king";
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+if (cardsInPlay.length === 2)
+	{
+		if (cardsInPlay[0] == cardsInPlay[1])
+{
+alert("you found a match!");
+
+}
+else
+{
+	alert("sorry try again");
+
+}
+}
